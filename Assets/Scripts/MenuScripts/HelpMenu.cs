0748878bbc816d934a6b272df2a33228f3317456
Maciej4 <m@maciej.me>
2020-07-31 17:56:19 -0700

    add new help menu

---
+++ Assets/Scripts/MenuScripts/HelpMenu.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HelpMenu : MonoBehaviour
+{
+    public Text f1ForHelpText;
+
+    private readonly Vector3 downPosition = new Vector3(0f, -600f, 0f);
+    private RectTransform rectTransform;
+
+    private bool helpMenuState = false;
+
+    private void Start()
+    {
+        rectTransform = GetComponent<RectTransform>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F1)) 
+        {
+            ToggleHelpMenu();
+        }
+    }
+
+    public void ToggleHelpMenu()
+    {
+        if (Input.GetKey(KeyCode.Return))
+        {
+            return;
+        }
+
+        if (helpMenuState) 
+        {
+            rectTransform.anchoredPosition = Vector3.zero;
+            f1ForHelpText.text = "F1 for help";
+            helpMenuState = false;
+            return;
+        }
+
+        rectTransform.anchoredPosition = downPosition;
+        f1ForHelpText.text = "F1 to hide";
+        helpMenuState = true;
+    }
+}
