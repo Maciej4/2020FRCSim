25166cd7d695a79a99e6becd355f2fd4effbe564
Maciej4 <m@maciej.me>
2020-08-01 21:10:49 -0700

    improve joystick support

--- Assets/Scripts/DriverStation/RobotJoysticks.cs
+++ Assets/Scripts/DriverStation/RobotJoysticks.cs
@@ -7,7 +7,7 @@ public class RobotJoysticks : MonoBehaviour
 {
     private RawImage RawImage;
 
-    private bool JoystickState;
+    private bool JoystickState = false;
 
     private readonly Color Red = new Color(255, 0, 0);
     private readonly Color Green = new Color(0, 255, 0);
@@ -15,6 +15,13 @@ public class RobotJoysticks : MonoBehaviour
     void Start()
     {
         RawImage = GetComponent<RawImage>();
+
+        bool IsJoystickConnected = Input.GetJoystickNames().Length == 0 || Input.GetJoystickNames()[0].Equals("");
+
+        if (IsJoystickConnected != JoystickState)
+        {
+            ToggleJoystickState();
+        }
     }
 
     void Update()
--- Assets/Scripts/HardwareScripts/Joystick.cs
+++ Assets/Scripts/HardwareScripts/Joystick.cs
@@ -8,6 +8,12 @@ public class Joystick : Hardware
     { KeyCode.Alpha0, KeyCode.Alpha1, KeyCode.Alpha2, KeyCode.Alpha3, KeyCode.Alpha4,
       KeyCode.Alpha5, KeyCode.Alpha6, KeyCode.Alpha7, KeyCode.Alpha8, KeyCode.Alpha9 };
 
+    private readonly List<KeyCode> joystickKeycodes = new List<KeyCode>
+    { KeyCode.JoystickButton0, KeyCode.JoystickButton1, KeyCode.JoystickButton2, 
+      KeyCode.JoystickButton3, KeyCode.JoystickButton4, KeyCode.JoystickButton5, 
+      KeyCode.JoystickButton6, KeyCode.JoystickButton7, KeyCode.JoystickButton8, 
+      KeyCode.JoystickButton9 };
+
     public Joystick()
     {
         type = "Joystick";
@@ -35,10 +41,12 @@ public class Joystick : Hardware
     {
         doubles[0] = Input.GetAxis("Horizontal");
         doubles[1] = Input.GetAxis("Vertical");
+        doubles[2] = Input.GetAxis("Horizontal2");
+        doubles[3] = Input.GetAxis("Vertical2");
 
         for (int i = 0; i < booleans.Length; i++)
         {
-            booleans[i] = Input.GetKey(buttonKeycodes[i]);
+            booleans[i] = Input.GetKey(buttonKeycodes[i]) || Input.GetKey(joystickKeycodes[i]);
         }
     }
 }
